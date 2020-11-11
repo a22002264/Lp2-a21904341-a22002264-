da6699965b093291ec3fbd3c312d6ae450a94ab3
a22002264 <a22002264@alunos.ulht>
2020-11-11 11:33:42 +0000

    Adicionei o ficheiro main e as suas classes

---
+++ src/pt/ulusofona/lp2/theWalkingDEISIGame/Main.java
@@ -0,0 +1,101 @@
+package pt.ulusofona.lp2.theWalkingDEISIGame;
+
+import pt.ulusofona.lp2.theWalkingDEISIGame.Humano;
+
+import java.io.File;
+import java.util.List;
+
+public class Main {
+
+
+
+    public boolean startGame(File ficheiroInicial){
+        return true;
+    }
+
+    public int[] getWorldSize(){
+        return null;
+    }
+
+    public int getInitialTeam(){
+        return 1;
+    }
+
+    public List<Humano> getHumans(){
+        return null;
+    }
+
+
+    public List<Zombie> getZombies(){
+        return null;
+    }
+
+    public boolean move(int xO, int yO, int xD, int yD){
+        return true;
+    }
+
+    public boolean gameIsOver(){
+        return true;
+    }
+
+    public List<String> getAuthors(){
+        return null ;
+    }
+
+    public int getCurrentTeamId(){
+        return 1;
+    }
+
+    public int getElementId(int x, int y){
+        return 1;
+    }
+
+
+
+    public List<String> getSurvivors(){
+
+        return null;
+    }
+
+    public boolean isDay(){
+
+
+        return true;
+    }
+
+    public boolean hasEquipment(int creatureId, int equipmentTypeId){
+        return true;
+    }
+
+    public int getId(){
+        return 1;
+    }
+
+    public String getImagePNG(){
+        return " ";
+    }
+
+
+
+    public String toString(){
+        return " ";
+    }
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+
+
+}
