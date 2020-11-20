ae6709e0978c899a906b51459ffa77822af78a82
a22002264 <a22002264@alunos.ulht>
2020-11-20 21:34:28 +0000

    Problema quando humano agarra equipamento.

--- src/pt/ulusofona/lp2/theWalkingDEISIGame/Zombie.java
+++ src/pt/ulusofona/lp2/theWalkingDEISIGame/Zombie.java
@@ -21,7 +21,7 @@ public class Zombie {
     }
     public String toString() {
 
-        return idCriatura + " | " + "Zombie" +" | " + "Os Outros" + " | " + nomeCriatura + totalEquipDestrui + " @ (" + x + ", " + y + ")";
+        return idCriatura + " | " + "Zombie" +" | " + "Os Outros" + " | " + nomeCriatura +"  " + totalEquipDestrui + " @ (" + x + ", " + y + ")";
     }
     public int getTipo() {
         return idTipo;
